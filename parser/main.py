8d275397d9de55a9105ef6dfed34a6af2066fd7d
Bogdan <bogdankhamidullin@gmail.com>
2024-12-18 20:37:49 +0000

    combined parser functions

--- parser/main.py
+++ parser/main.py
@@ -1,17 +1,17 @@
-import steam_parser
-import zaka_zaka_parser
-import gabestore_parser
-import steampay_parser
 import db
+import time
+from scrapers import steam_parser
+from scrapers.other_shops_parser import find_game_price_and_link
 
 data_base = db.DataBase('db', 'user', 'user_password', 'game_info')
 while True:
     games_with_prices = steam_parser.get_top_games_on_steam_with_special_prices()
     place_in_top = 1
     for game in games_with_prices:
-        game.zaka_zaka_price, game.zaka_zaka_link = zaka_zaka_parser.find_game_price_and_link(game.name)
-        game.gabe_store_price, game.gabe_store_link = gabestore_parser.find_game_price_and_link(game.name)
-        game.steam_pay_price, game.steam_pay_link = steampay_parser.find_game_price_and_link(game.name)
+
+        (game.zaka_zaka_price, game.zaka_zaka_link), (game.gabe_store_price, game.gabe_store_link), (game.steam_pay_price, game.steam_pay_link) = \
+        find_game_price_and_link(game.name, "zaka_zaka"), find_game_price_and_link(game.name, "gabe_store"), find_game_price_and_link(game.name, "steam_pay")
+
         data_base.update_game_prices(game, place_in_top)
         print("Updated info about game:", end="\t")
         data_base.add_game_image(game)
@@ -25,3 +25,5 @@ while True:
               game.gabe_store_price,
               sep="\t")
         place_in_top += 1
+
+    time.sleep(60 * 60)
