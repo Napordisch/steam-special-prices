18091824cf4aa365b64bfc6e9a6a69434b61379c
Bogdan <khamidullin.bogdan@gmail.com>
2024-12-18 16:24:14 +0500

    returning database data

--- website/db.py
+++ website/db.py
@@ -1,3 +1,54 @@
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, jsonify
+import mysql.connector
 
-db = SQLAlchemy()
+db_config = {
+    'host': 'localhost',
+    'user': 'user',
+    'password': 'user_password',
+    'database': 'game_info'
+}
+
+def query_db(query:str):
+    try:
+        # Connect to the database
+        connection = mysql.connector.connect(**db_config)
+        cursor = connection.cursor(dictionary=True)
+
+        # Execute a query
+        cursor.execute(query)
+        results = cursor.fetchall()
+        print (f"querying db: \"{query}\" ")
+        return (results)
+
+    except mysql.connector.Error as e:
+        print(({"error": str(e)}))
+
+    finally:
+        # Close connections
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+#
+# if __name__ == "__main__":
+#     try:
+#         # Connect to the database
+#         connection = mysql.connector.connect(**db_config)
+#         cursor = connection.cursor(dictionary=True)
+#
+#         # Execute a query
+#         query = "SELECT * FROM game_prices"
+#         cursor.execute(query)
+#         results = cursor.fetchall()
+#         for i in results:
+#             print(i)
+#
+#         print((results))
+#
+#     except mysql.connector.Error as e:
+#         print(({"error": str(e)}))
+#
+#     finally:
+#         # Close connections
+#         if connection.is_connected():
+#             cursor.close()
+#             connection.close()
--- website/main.py
+++ website/main.py
@@ -1,11 +1,27 @@
-from flask import Flask, render_template
-from markupsafe import escape
+from flask import Flask, render_template, jsonify
+from db import query_db
 
 app = Flask(__name__)
 
 @app.route('/')
-def show_all_games():
+def show_all_games_page():
     return render_template("games_page.html")
 
+@app.route('/all-games-prices')
+def get_games_prices():
+    games_with_prices_and_images = query_db("SELECT * FROM game_prices")
+    for game in games_with_prices_and_images:
+        image_link_dict = query_db(f"SELECT image_link FROM game_images WHERE id = \"{game['id']}\"")[0]
+        print(image_link_dict)
+        game.update(image_link_dict)
+    return jsonify(games_with_prices_and_images)
+
+@app.route('/game-info')
+def get_game_with_prices():
+    pass
+
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
+
